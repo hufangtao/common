53b1097ccf53a7d92e6874becfb8beb9baef8127
hufangtao <hufangta@qq.com>
2019-01-26 11:13:33 +0800

    all

--- proto/NullPmd.pb.cc
+++ proto/NullPmd.pb.cc
@@ -30,11 +30,11 @@ class NetPackageNullPmd_C2SDefaultTypeInternal {
   ::google::protobuf::internal::ExplicitlyConstructed<NetPackageNullPmd_C2S>
       _instance;
 } _NetPackageNullPmd_C2S_default_instance_;
-class NetTickNullPmd_SCDefaultTypeInternal {
+class NetTickNullPmd_S2CDefaultTypeInternal {
  public:
-  ::google::protobuf::internal::ExplicitlyConstructed<NetTickNullPmd_SC>
+  ::google::protobuf::internal::ExplicitlyConstructed<NetTickNullPmd_S2C>
       _instance;
-} _NetTickNullPmd_SC_default_instance_;
+} _NetTickNullPmd_S2C_default_instance_;
 }  // namespace PlatPmd
 namespace protobuf_NullPmd_2eproto {
 static void InitDefaultsNullPmd() {
@@ -65,24 +65,24 @@ static void InitDefaultsNetPackageNullPmd_C2S() {
 ::google::protobuf::internal::SCCInfo<0> scc_info_NetPackageNullPmd_C2S =
     {{ATOMIC_VAR_INIT(::google::protobuf::internal::SCCInfoBase::kUninitialized), 0, InitDefaultsNetPackageNullPmd_C2S}, {}};
 
-static void InitDefaultsNetTickNullPmd_SC() {
+static void InitDefaultsNetTickNullPmd_S2C() {
   GOOGLE_PROTOBUF_VERIFY_VERSION;
 
   {
-    void* ptr = &::PlatPmd::_NetTickNullPmd_SC_default_instance_;
-    new (ptr) ::PlatPmd::NetTickNullPmd_SC();
+    void* ptr = &::PlatPmd::_NetTickNullPmd_S2C_default_instance_;
+    new (ptr) ::PlatPmd::NetTickNullPmd_S2C();
     ::google::protobuf::internal::OnShutdownDestroyMessage(ptr);
   }
-  ::PlatPmd::NetTickNullPmd_SC::InitAsDefaultInstance();
+  ::PlatPmd::NetTickNullPmd_S2C::InitAsDefaultInstance();
 }
 
-::google::protobuf::internal::SCCInfo<0> scc_info_NetTickNullPmd_SC =
-    {{ATOMIC_VAR_INIT(::google::protobuf::internal::SCCInfoBase::kUninitialized), 0, InitDefaultsNetTickNullPmd_SC}, {}};
+::google::protobuf::internal::SCCInfo<0> scc_info_NetTickNullPmd_S2C =
+    {{ATOMIC_VAR_INIT(::google::protobuf::internal::SCCInfoBase::kUninitialized), 0, InitDefaultsNetTickNullPmd_S2C}, {}};
 
 void InitDefaults() {
   ::google::protobuf::internal::InitSCC(&scc_info_NullPmd.base);
   ::google::protobuf::internal::InitSCC(&scc_info_NetPackageNullPmd_C2S.base);
-  ::google::protobuf::internal::InitSCC(&scc_info_NetTickNullPmd_SC.base);
+  ::google::protobuf::internal::InitSCC(&scc_info_NetTickNullPmd_S2C.base);
 }
 
 ::google::protobuf::Metadata file_level_metadata[3];
@@ -108,23 +108,23 @@ const ::google::protobuf::uint32 TableStruct::offsets[] GOOGLE_PROTOBUF_ATTRIBUT
   GOOGLE_PROTOBUF_GENERATED_MESSAGE_FIELD_OFFSET(::PlatPmd::NetPackageNullPmd_C2S, bitmask_),
   GOOGLE_PROTOBUF_GENERATED_MESSAGE_FIELD_OFFSET(::PlatPmd::NetPackageNullPmd_C2S, time_),
   ~0u,  // no _has_bits_
-  GOOGLE_PROTOBUF_GENERATED_MESSAGE_FIELD_OFFSET(::PlatPmd::NetTickNullPmd_SC, _internal_metadata_),
+  GOOGLE_PROTOBUF_GENERATED_MESSAGE_FIELD_OFFSET(::PlatPmd::NetTickNullPmd_S2C, _internal_metadata_),
   ~0u,  // no _extensions_
   ~0u,  // no _oneof_case_
   ~0u,  // no _weak_field_map_
-  GOOGLE_PROTOBUF_GENERATED_MESSAGE_FIELD_OFFSET(::PlatPmd::NetTickNullPmd_SC, requesttime_),
-  GOOGLE_PROTOBUF_GENERATED_MESSAGE_FIELD_OFFSET(::PlatPmd::NetTickNullPmd_SC, mytime_),
+  GOOGLE_PROTOBUF_GENERATED_MESSAGE_FIELD_OFFSET(::PlatPmd::NetTickNullPmd_S2C, requesttime_),
+  GOOGLE_PROTOBUF_GENERATED_MESSAGE_FIELD_OFFSET(::PlatPmd::NetTickNullPmd_S2C, mytime_),
 };
 static const ::google::protobuf::internal::MigrationSchema schemas[] GOOGLE_PROTOBUF_ATTRIBUTE_SECTION_VARIABLE(protodesc_cold) = {
   { 0, -1, sizeof(::PlatPmd::NullPmd)},
   { 5, -1, sizeof(::PlatPmd::NetPackageNullPmd_C2S)},
-  { 18, -1, sizeof(::PlatPmd::NetTickNullPmd_SC)},
+  { 18, -1, sizeof(::PlatPmd::NetTickNullPmd_S2C)},
 };
 
 static ::google::protobuf::Message const * const file_default_instances[] = {
   reinterpret_cast<const ::google::protobuf::Message*>(&::PlatPmd::_NullPmd_default_instance_),
   reinterpret_cast<const ::google::protobuf::Message*>(&::PlatPmd::_NetPackageNullPmd_C2S_default_instance_),
-  reinterpret_cast<const ::google::protobuf::Message*>(&::PlatPmd::_NetTickNullPmd_SC_default_instance_),
+  reinterpret_cast<const ::google::protobuf::Message*>(&::PlatPmd::_NetTickNullPmd_S2C_default_instance_),
 };
 
 void protobuf_AssignDescriptors() {
@@ -154,11 +154,11 @@ void AddDescriptorsImpl() {
       "S\022\r\n\005byCmd\030\001 \001(\005\022\017\n\007byParam\030\002 \001(\005\022\013\n\003seq"
       "\030\003 \001(\005\022\013\n\003fid\030\004 \001(\003\022\014\n\004data\030\005 \001(\014\022\021\n\tpro"
       "totype\030\006 \001(\003\022\017\n\007bitmask\030\007 \001(\005\022\014\n\004time\030\010 "
-      "\001(\005\"8\n\021NetTickNullPmd_SC\022\023\n\013requesttime\030"
-      "\001 \001(\005\022\016\n\006mytime\030\002 \001(\005b\006proto3"
+      "\001(\005\"9\n\022NetTickNullPmd_S2C\022\023\n\013requesttime"
+      "\030\001 \001(\005\022\016\n\006mytime\030\002 \001(\005b\006proto3"
   };
   ::google::protobuf::DescriptorPool::InternalAddGeneratedFile(
-      descriptor, 309);
+      descriptor, 310);
   ::google::protobuf::MessageFactory::InternalRegisterGeneratedFile(
     "NullPmd.proto", &protobuf_RegisterTypes);
 }
@@ -869,60 +869,60 @@ void NetPackageNullPmd_C2S::InternalSwap(NetPackageNullPmd_C2S* other) {
 
 // ===================================================================
 
-void NetTickNullPmd_SC::InitAsDefaultInstance() {
+void NetTickNullPmd_S2C::InitAsDefaultInstance() {
 }
 #if !defined(_MSC_VER) || _MSC_VER >= 1900
-const int NetTickNullPmd_SC::kRequesttimeFieldNumber;
-const int NetTickNullPmd_SC::kMytimeFieldNumber;
+const int NetTickNullPmd_S2C::kRequesttimeFieldNumber;
+const int NetTickNullPmd_S2C::kMytimeFieldNumber;
 #endif  // !defined(_MSC_VER) || _MSC_VER >= 1900
 
-NetTickNullPmd_SC::NetTickNullPmd_SC()
+NetTickNullPmd_S2C::NetTickNullPmd_S2C()
   : ::google::protobuf::Message(), _internal_metadata_(NULL) {
   ::google::protobuf::internal::InitSCC(
-      &protobuf_NullPmd_2eproto::scc_info_NetTickNullPmd_SC.base);
+      &protobuf_NullPmd_2eproto::scc_info_NetTickNullPmd_S2C.base);
   SharedCtor();
-  // @@protoc_insertion_point(constructor:PlatPmd.NetTickNullPmd_SC)
+  // @@protoc_insertion_point(constructor:PlatPmd.NetTickNullPmd_S2C)
 }
-NetTickNullPmd_SC::NetTickNullPmd_SC(const NetTickNullPmd_SC& from)
+NetTickNullPmd_S2C::NetTickNullPmd_S2C(const NetTickNullPmd_S2C& from)
   : ::google::protobuf::Message(),
       _internal_metadata_(NULL) {
   _internal_metadata_.MergeFrom(from._internal_metadata_);
   ::memcpy(&requesttime_, &from.requesttime_,
     static_cast<size_t>(reinterpret_cast<char*>(&mytime_) -
     reinterpret_cast<char*>(&requesttime_)) + sizeof(mytime_));
-  // @@protoc_insertion_point(copy_constructor:PlatPmd.NetTickNullPmd_SC)
+  // @@protoc_insertion_point(copy_constructor:PlatPmd.NetTickNullPmd_S2C)
 }
 
-void NetTickNullPmd_SC::SharedCtor() {
+void NetTickNullPmd_S2C::SharedCtor() {
   ::memset(&requesttime_, 0, static_cast<size_t>(
       reinterpret_cast<char*>(&mytime_) -
       reinterpret_cast<char*>(&requesttime_)) + sizeof(mytime_));
 }
 
-NetTickNullPmd_SC::~NetTickNullPmd_SC() {
-  // @@protoc_insertion_point(destructor:PlatPmd.NetTickNullPmd_SC)
+NetTickNullPmd_S2C::~NetTickNullPmd_S2C() {
+  // @@protoc_insertion_point(destructor:PlatPmd.NetTickNullPmd_S2C)
   SharedDtor();
 }
 
-void NetTickNullPmd_SC::SharedDtor() {
+void NetTickNullPmd_S2C::SharedDtor() {
 }
 
-void NetTickNullPmd_SC::SetCachedSize(int size) const {
+void NetTickNullPmd_S2C::SetCachedSize(int size) const {
   _cached_size_.Set(size);
 }
-const ::google::protobuf::Descriptor* NetTickNullPmd_SC::descriptor() {
+const ::google::protobuf::Descriptor* NetTickNullPmd_S2C::descriptor() {
   ::protobuf_NullPmd_2eproto::protobuf_AssignDescriptorsOnce();
   return ::protobuf_NullPmd_2eproto::file_level_metadata[kIndexInFileMessages].descriptor;
 }
 
-const NetTickNullPmd_SC& NetTickNullPmd_SC::default_instance() {
-  ::google::protobuf::internal::InitSCC(&protobuf_NullPmd_2eproto::scc_info_NetTickNullPmd_SC.base);
+const NetTickNullPmd_S2C& NetTickNullPmd_S2C::default_instance() {
+  ::google::protobuf::internal::InitSCC(&protobuf_NullPmd_2eproto::scc_info_NetTickNullPmd_S2C.base);
   return *internal_default_instance();
 }
 
 
-void NetTickNullPmd_SC::Clear() {
-// @@protoc_insertion_point(message_clear_start:PlatPmd.NetTickNullPmd_SC)
+void NetTickNullPmd_S2C::Clear() {
+// @@protoc_insertion_point(message_clear_start:PlatPmd.NetTickNullPmd_S2C)
   ::google::protobuf::uint32 cached_has_bits = 0;
   // Prevent compiler warnings about cached_has_bits being unused
   (void) cached_has_bits;
@@ -933,11 +933,11 @@ void NetTickNullPmd_SC::Clear() {
   _internal_metadata_.Clear();
 }
 
-bool NetTickNullPmd_SC::MergePartialFromCodedStream(
+bool NetTickNullPmd_S2C::MergePartialFromCodedStream(
     ::google::protobuf::io::CodedInputStream* input) {
 #define DO_(EXPRESSION) if (!GOOGLE_PREDICT_TRUE(EXPRESSION)) goto failure
   ::google::protobuf::uint32 tag;
-  // @@protoc_insertion_point(parse_start:PlatPmd.NetTickNullPmd_SC)
+  // @@protoc_insertion_point(parse_start:PlatPmd.NetTickNullPmd_S2C)
   for (;;) {
     ::std::pair<::google::protobuf::uint32, bool> p = input->ReadTagWithCutoffNoLastTag(127u);
     tag = p.first;
@@ -983,17 +983,17 @@ bool NetTickNullPmd_SC::MergePartialFromCodedStream(
     }
   }
 success:
-  // @@protoc_insertion_point(parse_success:PlatPmd.NetTickNullPmd_SC)
+  // @@protoc_insertion_point(parse_success:PlatPmd.NetTickNullPmd_S2C)
   return true;
 failure:
-  // @@protoc_insertion_point(parse_failure:PlatPmd.NetTickNullPmd_SC)
+  // @@protoc_insertion_point(parse_failure:PlatPmd.NetTickNullPmd_S2C)
   return false;
 #undef DO_
 }
 
-void NetTickNullPmd_SC::SerializeWithCachedSizes(
+void NetTickNullPmd_S2C::SerializeWithCachedSizes(
     ::google::protobuf::io::CodedOutputStream* output) const {
-  // @@protoc_insertion_point(serialize_start:PlatPmd.NetTickNullPmd_SC)
+  // @@protoc_insertion_point(serialize_start:PlatPmd.NetTickNullPmd_S2C)
   ::google::protobuf::uint32 cached_has_bits = 0;
   (void) cached_has_bits;
 
@@ -1011,13 +1011,13 @@ void NetTickNullPmd_SC::SerializeWithCachedSizes(
     ::google::protobuf::internal::WireFormat::SerializeUnknownFields(
         (::google::protobuf::internal::GetProto3PreserveUnknownsDefault()   ? _internal_metadata_.unknown_fields()   : _internal_metadata_.default_instance()), output);
   }
-  // @@protoc_insertion_point(serialize_end:PlatPmd.NetTickNullPmd_SC)
+  // @@protoc_insertion_point(serialize_end:PlatPmd.NetTickNullPmd_S2C)
 }
 
-::google::protobuf::uint8* NetTickNullPmd_SC::InternalSerializeWithCachedSizesToArray(
+::google::protobuf::uint8* NetTickNullPmd_S2C::InternalSerializeWithCachedSizesToArray(
     bool deterministic, ::google::protobuf::uint8* target) const {
   (void)deterministic; // Unused
-  // @@protoc_insertion_point(serialize_to_array_start:PlatPmd.NetTickNullPmd_SC)
+  // @@protoc_insertion_point(serialize_to_array_start:PlatPmd.NetTickNullPmd_S2C)
   ::google::protobuf::uint32 cached_has_bits = 0;
   (void) cached_has_bits;
 
@@ -1035,12 +1035,12 @@ void NetTickNullPmd_SC::SerializeWithCachedSizes(
     target = ::google::protobuf::internal::WireFormat::SerializeUnknownFieldsToArray(
         (::google::protobuf::internal::GetProto3PreserveUnknownsDefault()   ? _internal_metadata_.unknown_fields()   : _internal_metadata_.default_instance()), target);
   }
-  // @@protoc_insertion_point(serialize_to_array_end:PlatPmd.NetTickNullPmd_SC)
+  // @@protoc_insertion_point(serialize_to_array_end:PlatPmd.NetTickNullPmd_S2C)
   return target;
 }
 
-size_t NetTickNullPmd_SC::ByteSizeLong() const {
-// @@protoc_insertion_point(message_byte_size_start:PlatPmd.NetTickNullPmd_SC)
+size_t NetTickNullPmd_S2C::ByteSizeLong() const {
+// @@protoc_insertion_point(message_byte_size_start:PlatPmd.NetTickNullPmd_S2C)
   size_t total_size = 0;
 
   if ((_internal_metadata_.have_unknown_fields() &&  ::google::protobuf::internal::GetProto3PreserveUnknownsDefault())) {
@@ -1067,23 +1067,23 @@ size_t NetTickNullPmd_SC::ByteSizeLong() const {
   return total_size;
 }
 
-void NetTickNullPmd_SC::MergeFrom(const ::google::protobuf::Message& from) {
-// @@protoc_insertion_point(generalized_merge_from_start:PlatPmd.NetTickNullPmd_SC)
+void NetTickNullPmd_S2C::MergeFrom(const ::google::protobuf::Message& from) {
+// @@protoc_insertion_point(generalized_merge_from_start:PlatPmd.NetTickNullPmd_S2C)
   GOOGLE_DCHECK_NE(&from, this);
-  const NetTickNullPmd_SC* source =
-      ::google::protobuf::internal::DynamicCastToGenerated<const NetTickNullPmd_SC>(
+  const NetTickNullPmd_S2C* source =
+      ::google::protobuf::internal::DynamicCastToGenerated<const NetTickNullPmd_S2C>(
           &from);
   if (source == NULL) {
-  // @@protoc_insertion_point(generalized_merge_from_cast_fail:PlatPmd.NetTickNullPmd_SC)
+  // @@protoc_insertion_point(generalized_merge_from_cast_fail:PlatPmd.NetTickNullPmd_S2C)
     ::google::protobuf::internal::ReflectionOps::Merge(from, this);
   } else {
-  // @@protoc_insertion_point(generalized_merge_from_cast_success:PlatPmd.NetTickNullPmd_SC)
+  // @@protoc_insertion_point(generalized_merge_from_cast_success:PlatPmd.NetTickNullPmd_S2C)
     MergeFrom(*source);
   }
 }
 
-void NetTickNullPmd_SC::MergeFrom(const NetTickNullPmd_SC& from) {
-// @@protoc_insertion_point(class_specific_merge_from_start:PlatPmd.NetTickNullPmd_SC)
+void NetTickNullPmd_S2C::MergeFrom(const NetTickNullPmd_S2C& from) {
+// @@protoc_insertion_point(class_specific_merge_from_start:PlatPmd.NetTickNullPmd_S2C)
   GOOGLE_DCHECK_NE(&from, this);
   _internal_metadata_.MergeFrom(from._internal_metadata_);
   ::google::protobuf::uint32 cached_has_bits = 0;
@@ -1097,36 +1097,36 @@ void NetTickNullPmd_SC::MergeFrom(const NetTickNullPmd_SC& from) {
   }
 }
 
-void NetTickNullPmd_SC::CopyFrom(const ::google::protobuf::Message& from) {
-// @@protoc_insertion_point(generalized_copy_from_start:PlatPmd.NetTickNullPmd_SC)
+void NetTickNullPmd_S2C::CopyFrom(const ::google::protobuf::Message& from) {
+// @@protoc_insertion_point(generalized_copy_from_start:PlatPmd.NetTickNullPmd_S2C)
   if (&from == this) return;
   Clear();
   MergeFrom(from);
 }
 
-void NetTickNullPmd_SC::CopyFrom(const NetTickNullPmd_SC& from) {
-// @@protoc_insertion_point(class_specific_copy_from_start:PlatPmd.NetTickNullPmd_SC)
+void NetTickNullPmd_S2C::CopyFrom(const NetTickNullPmd_S2C& from) {
+// @@protoc_insertion_point(class_specific_copy_from_start:PlatPmd.NetTickNullPmd_S2C)
   if (&from == this) return;
   Clear();
   MergeFrom(from);
 }
 
-bool NetTickNullPmd_SC::IsInitialized() const {
+bool NetTickNullPmd_S2C::IsInitialized() const {
   return true;
 }
 
-void NetTickNullPmd_SC::Swap(NetTickNullPmd_SC* other) {
+void NetTickNullPmd_S2C::Swap(NetTickNullPmd_S2C* other) {
   if (other == this) return;
   InternalSwap(other);
 }
-void NetTickNullPmd_SC::InternalSwap(NetTickNullPmd_SC* other) {
+void NetTickNullPmd_S2C::InternalSwap(NetTickNullPmd_S2C* other) {
   using std::swap;
   swap(requesttime_, other->requesttime_);
   swap(mytime_, other->mytime_);
   _internal_metadata_.Swap(&other->_internal_metadata_);
 }
 
-::google::protobuf::Metadata NetTickNullPmd_SC::GetMetadata() const {
+::google::protobuf::Metadata NetTickNullPmd_S2C::GetMetadata() const {
   protobuf_NullPmd_2eproto::protobuf_AssignDescriptorsOnce();
   return ::protobuf_NullPmd_2eproto::file_level_metadata[kIndexInFileMessages];
 }
@@ -1142,8 +1142,8 @@ template<> GOOGLE_PROTOBUF_ATTRIBUTE_NOINLINE ::PlatPmd::NullPmd* Arena::CreateM
 template<> GOOGLE_PROTOBUF_ATTRIBUTE_NOINLINE ::PlatPmd::NetPackageNullPmd_C2S* Arena::CreateMaybeMessage< ::PlatPmd::NetPackageNullPmd_C2S >(Arena* arena) {
   return Arena::CreateInternal< ::PlatPmd::NetPackageNullPmd_C2S >(arena);
 }
-template<> GOOGLE_PROTOBUF_ATTRIBUTE_NOINLINE ::PlatPmd::NetTickNullPmd_SC* Arena::CreateMaybeMessage< ::PlatPmd::NetTickNullPmd_SC >(Arena* arena) {
-  return Arena::CreateInternal< ::PlatPmd::NetTickNullPmd_SC >(arena);
+template<> GOOGLE_PROTOBUF_ATTRIBUTE_NOINLINE ::PlatPmd::NetTickNullPmd_S2C* Arena::CreateMaybeMessage< ::PlatPmd::NetTickNullPmd_S2C >(Arena* arena) {
+  return Arena::CreateInternal< ::PlatPmd::NetTickNullPmd_S2C >(arena);
 }
 }  // namespace protobuf
 }  // namespace google
